e1a98a49d77811c0366769cde43c89ad633eb211
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2026-07-27 20:21:04 +1000

    create mode for Idea and add some test

--- tests/Pest.php
+++ tests/Pest.php
@@ -17,8 +17,8 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
-    ->in('Feature');
+    ->use(RefreshDatabase::class)
+    ->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ pest()->extend(TestCase::class)
 |
 */
 
-expect()->extend('toBeOne', fn () => $this->toBe(1));
+expect()->extend('toBeOne', fn() => $this->toBe(1));
 
 /*
 |--------------------------------------------------------------------------
